15b85d90a28a20a778d986054f9670e5b0e2c60b
Marcin Bogus <marcin@creative-bay.com>
2023-10-21 17:26:10 +0200

    Initial commit

---
+++ wp-content/plugins/okapi-blocks/okapi-blocks.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Okapi Blocks
+Description: A list of blocks used for okapi marketing websites
+Version: 1.0
+Author: Creative Bay
+*/
---
+++ wp-content/plugins/test-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '647f581a301ebf05c8ae');
