785838f571c0939b0a66eff09ecfdaf790a3a64d
Marcin Bogus <marcin@creative-bay.com>
2023-10-25 20:08:22 +0200

    Update theme

---
+++ wp-content/themes/okapi/patterns/footer.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Title: footer
+ * Slug: okapi/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"var:preset|spacing|40"},"blockGap":"0"}},"backgroundColor":"light-blue","layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group has-light-blue-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Are you passionate about literacy?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"8px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>Hear from literacy's leading voices and get classroom tools and more</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:create-block/test-block -->
+<div class="wp-block-create-block-test-block"><form class="newsletter-form" target="_blank" action="https://myokapi.com/newsletter-form" method="GET"><input name="email" placeholder="Enter your email"/><button type="submit">Join us</button></form></div>
+<!-- /wp:create-block/test-block --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"color":{"background":"#3069b3"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"textColor":"base","layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-base-color has-text-color has-background has-small-font-size" style="background-color:#3069b3;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":207,"width":"180px","sizeSlug":"full","linkDestination":"custom","className":"footer-logo"} -->
+<figure class="wp-block-image size-full is-resized footer-logo"><a href="https://myokapi.com" target="_blank" rel="noreferrer noopener"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo-footer.svg" alt="" class="wp-image-207" style="width:180px"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Flexible literacy <br>solutions built on best-practice<br>instruction.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><a rel="noreferrer noopener" href="https://myokapi.com/programs" data-type="link" data-id="https://myokapi.com/programs" target="_blank">Programs</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a rel="noreferrer noopener" href="https://myokapi.com/digital-samples" data-type="link" data-id="https://myokapi.com/digital-samples" target="_blank">Samples</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><a href="https://myokapi.com/literacy-solutions/about-us" target="_blank" rel="noreferrer noopener">About Us</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a rel="noreferrer noopener" href="https://myokapi.com/teaching/contact-us" data-type="link" data-id="https://myokapi.com/teaching/contact-us" target="_blank">Contact</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"fontSize":"small"} -->
+<div class="wp-block-column has-small-font-size"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><a href="https://myokapi.com/privacy-policy" data-type="link" data-id="https://myokapi.com/privacy-policy" target="_blank" rel="noreferrer noopener">Privacy Policy</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><a href="https://myokapi.com/terms-of-use" data-type="link" data-id="https://myokapi.com/terms-of-use" target="_blank" rel="noreferrer noopener">Terms of Use</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">© 2023 <a href="https://myokapi.com" target="_blank" rel="noreferrer noopener">Okapi</a>. All Rights Reserved.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ wp-content/themes/okapi/patterns/page-with-sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Page with sidebar
+ * Slug: okapi/page-with-sidebar
+ */
+?>
+<!-- wp:columns {"className":"page-with-sidebar"} -->
+<div class="wp-block-columns page-with-sidebar">
+    <!-- wp:column {"width":"100%","className":"main-content","layout":{"type":"default"}} -->
+    <div class="wp-block-column main-content" style="flex-basis:100%"></div> <!-- /wp:column -->
+    <!-- wp:column {"width":"18rem","className":"sidebar"} -->
+    <div class="wp-block-column sidebar" style="flex-basis:18rem"></div> <!-- /wp:column -->
+</div> <!-- /wp:columns -->
