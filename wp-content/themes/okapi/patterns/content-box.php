c83f125777906e30af69a1f84e0225586e2f6de8
Marcin Bogus <marcin@creative-bay.com>
2023-11-05 11:18:07 +0100

    Add content box template part

---
+++ wp-content/themes/okapi/patterns/content-box.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Content Box
+ * Slug: okapi/content-box
+ */
+?>
+<!-- wp:group {"className":"content-box","layout":{"type":"constrained"}} -->
+<div class="wp-block-group content-box"><!-- wp:heading {"level":3} -->
+    <h3 class="wp-block-heading">Headline</h3> <!-- /wp:heading --> <!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column description"><!-- wp:paragraph -->
+            <p>Description</p> <!-- /wp:paragraph -->
+        </div> <!-- /wp:column --> <!-- wp:column -->
+        <div class="wp-block-column call-to-action"><!-- wp:buttons -->
+            <div class="wp-block-buttons"><!-- wp:button -->
+                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn More</a></div>
+                <!-- /wp:button -->
+            </div> <!-- /wp:buttons -->
+        </div> <!-- /wp:column -->
+    </div> <!-- /wp:columns -->
+</div> <!-- /wp:group -->
